ffd8ab41903bb8193e6f97fe1e9453aa00a62edb
Kevin-tre <kevintrepied@gmail.com>
2024-08-13 18:02:08 +0200

    fix slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,19 +12,21 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length-1 ? index + 1 : 0),
       5000
     );
   };
   useEffect(() => {
-    nextCard();
+    if (byDateDesc) {
+      nextCard();
+    }
+    
   });
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <div  key={`${event.title}`}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -34,6 +36,7 @@ const Slider = () => {
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
+          
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
@@ -42,15 +45,16 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${_.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
